60e02adfb8ace7908109687a2853d1d58825fd7e
Adam Urbina <adamurbina@gmail.com>
2018-10-02 14:15:00 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -7,19 +7,4 @@ class City < ActiveRecord::Base
   include Helper::InstanceMethods
 
   after_initialize :make_method
-
-  # def city_openings(start_date, end_date)
-  #
-  #   start_date = Date.parse(start_date)
-  #   end_date = Date.parse(end_date)
-  #   available_listings = self.listings.all
-  #
-  #   self.reservations.all.each do |res|
-  #     if !(res.checkout <= start_date || res.checkin >= end_date)
-  #       available_listings.delete(res.listing)
-  #     end
-  #   end
-  #   available_listings
-  # end
-
 end
