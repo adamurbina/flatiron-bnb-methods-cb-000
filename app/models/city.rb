71b1f84c6351da7557c8cdc5850ec1015709feeb
Adam Urbina <adamurbina@gmail.com>
2018-10-02 13:08:06 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -7,12 +7,14 @@ class City < ActiveRecord::Base
   include Helper::InstanceMethods
 
   def city_openings(start_date, end_date)
+    
     start_date = Date.parse(start_date)
     end_date = Date.parse(end_date)
     city_reservations = self.reservations.all
     available_listings = []
+    
     city_reservations.each do |res|
-      available_listings << res.listing if (res.checkout < start_date || res.checkin > end_date)
+      available_listings << res.listing if (res.checkout <= start_date || res.checkin >= end_date)
     end
     available_listings.uniq
   end
